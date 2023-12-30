ea54c78943710338b01cc94aa99d87d048bbf843
Daniel Lewis <daniel.lewis@heimdalldev.com>
2023-12-30 18:41:23 +0100

    completed the hex and hsl displays

--- script.js
+++ script.js
@@ -7,61 +7,92 @@ var color2 = document.querySelector(".color2"); 				//colorinput right
 var body = document.getElementById("gradient"); 				//body
 var random1 = document.querySelector(".r1")						//random btn left
 var random2 = document.querySelector(".r2")						//random btn right
-var complimentaryBtn = document.querySelector(".complimantary") //complimantary color btn
+var complementaryBtn = document.querySelector(".complemantary") //complemantary color btn
 var contrastingBtn = document.querySelector(".contrasted")		//contrasted color btn
 
 
-// FUNCTIONS:
-
-
-function source = document.querySelector("div.source");
-
-source.addEventListener("copy", (event) => {
-  const selection = document.getSelection();
-  event.clipboardData.setData("text/plain", selection.toString().toUpperCase());
-  event.preventDefault();
-});
-
-
-function copyToClipboard(element) {
-    element.event.target = copy
-}
-
-
-	// Updates the background color based on color input elements
-
 function setGradient() {
-			body.style.background = "linear-gradient(to right, " 
-			+ color1.value 
-			+ ", " 
-			+ color2.value 
-			+ ")";
+    body.style.background = "linear-gradient(to right, " 
+    + color1.value 
+    + ", " 
+    + color2.value 
+    + ")";
 
-			//h3 element receives rgb color code for gradient. For users to copy.
-			cssRgb.textContent = body.style.background + ";";
-			cssHex.textContent = body.style.background + ";";
-			cssHsl.textContent = body.style.background + ";";
+
+     // Display RGB value
+    cssRgb.textContent = body.style.background + ";";
+
+    // Display Hex value
+    cssHex.textContent = "linear-gradient(to right, " + color1.value + " - " + color2.value + ";";
+
+    // Calculate and display HSL value
+    var hslColor1 = getHslFormat(color1.value);
+    var hslColor2 = getHslFormat(color2.value);
+    cssHsl.textContent = "linear-gradient(to right, " + hslColor1 + " - " + hslColor2 + ";";
 }
+
+
+
+
 
 
 	// Generate random hex code for color input
 
-function generateRandomHex(colorInput) {
+function generateRandomHex() {
 
 		var hexCode = "#";
-  		var hexValues = "0123456789abcdef";
+  	var hexValues = "0123456789abcdef";
 
-  		for (var i = 0; i < 6; i++) {
+  	for (var i = 0; i < 6; i++) {
     			hexCode += hexValues[(Math.floor(Math.random() * hexValues.length))];
-  		}
+  	}
 
-  		// Show hexcode in console. For easy Debugging.
-    	console.log(hexCode);
-
-
-        // Set the value of the provided color input
-  		return hexCode;
+  	return hexCode;
 }
+
+
+// Convertion function (This bit with help from chatGPT)
+
+// Function to get HSL format from a hex color code
+function getHslFormat(hex) {
+    // Remove the '#' from the beginning of the hex code
+    hex = hex.slice(1);
+
+    // Convert hex to RGB
+    var r = parseInt(hex.substring(0, 2), 16) / 255;
+    var g = parseInt(hex.substring(2, 4), 16) / 255;
+    var b = parseInt(hex.substring(4, 6), 16) / 255;
+
+    // Find the minimum and maximum values
+    var cMin = Math.min(r, g, b);
+    var cMax = Math.max(r, g, b);
+
+    // Calculate lightness
+    var l = (cMin + cMax) / 2;
+
+    // Calculate saturation
+    var delta = cMax - cMin;
+    var s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    // Calculate hue
+    var h;
+    if (delta === 0) {
+        h = 0;
+    } else if (cMax === r) {
+        h = ((g - b) / delta) % 6;
+    } else if (cMax === g) {
+        h = (b - r) / delta + 2;
+    } else {
+        h = (r - g) / delta + 4;
+    }
+
+    // Ensure hue is positive and convert it to degrees
+    h = Math.round((h * 60 + 360) % 360);
+
+    // Format HSL string
+    return "HSL(" + h + ", " + (s * 100) + "%, " + (l * 100) + "%)";
+}
+
 
 
 	// Adjust in realtime the background color
@@ -75,7 +106,7 @@ color2.addEventListener("input", setGradient);
 
 random1.addEventListener("click", function() {
   // Generate a random hex color for color1
-  var randomColor1 = generateRandomHex(color1);
+  var randomColor1 = generateRandomHex();
 
 
   // Set the value of color1 and update the background gradient
@@ -88,7 +119,7 @@ random1.addEventListener("click", function() {
 	
 random2.addEventListener("click", function() {
 	// Generate a random hex color for color2
-  var randomColor2 = generateRandomHex(color2);
+  var randomColor2 = generateRandomHex();
 
   // Set the value of color2 and update the background gradient
   color2.value = randomColor2;
