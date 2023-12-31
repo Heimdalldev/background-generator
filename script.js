19a619d49827ac9320ed0805fd5bca209ccad7d4
Daniel Lewis <daniel.lewis@heimdalldev.com>
2023-12-31 12:30:24 +0100

    refractored code to ES6 syntax. Fixed HSL decimal numbers issue for S and L. MAde similar change to H for similar language reasons.

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // VARIABLES: 
-var cssRgb = document.querySelector("h3"); 						//show rgb
-var cssHex = document.querySelector("h4"); 						//show hexcode
-var cssHsl = document.querySelector("h5"); 						//show hsl
-var color1 = document.querySelector(".color1"); 				//colorinput left
-var color2 = document.querySelector(".color2"); 				//colorinput right
-var body = document.getElementById("gradient"); 				//body
-var random1 = document.querySelector(".r1")						//random btn left
-var random2 = document.querySelector(".r2")						//random btn right
-var complementaryBtn = document.querySelector(".complemantary") //complemantary color btn
-var contrastingBtn = document.querySelector(".contrasted")		//contrasted color btn
+const cssRgb = document.querySelector("h3"); 						//show rgb
+const cssHex = document.querySelector("h4"); 						//show hexcode
+const cssHsl = document.querySelector("h5"); 						//show hsl
 
 
-function setGradient() {
+let color1 = document.querySelector(".color1"); 				//colorinput left
+let color2 = document.querySelector(".color2"); 				//colorinput right
+let body = document.getElementById("gradient"); 				//body
+let random1 = document.querySelector(".r1")						//random btn left
+let random2 = document.querySelector(".r2")						//random btn right
+let complementaryBtn = document.querySelector(".complemantary") //complemantary color btn
+let contrastingBtn = document.querySelector(".contrasted")		//contrasted color btn
+
+
+const setGradient = () => {
     body.style.background = "linear-gradient(to right, " 
     + color1.value 
     + ", " 
@@ -26,24 +28,20 @@ function setGradient() {
     cssHex.textContent = "linear-gradient(to right, " + color1.value + " - " + color2.value + ";";
 
     // Calculate and display HSL value
-    var hslColor1 = getHslFormat(color1.value);
-    var hslColor2 = getHslFormat(color2.value);
+    const hslColor1 = getHslFormat(color1.value);
+    const hslColor2 = getHslFormat(color2.value);
     cssHsl.textContent = "linear-gradient(to right, " + hslColor1 + " - " + hslColor2 + ";";
 }
 
 
-
-
-
-
 	// Generate random hex code for color input
 
-function generateRandomHex() {
+const generateRandomHex = () => {
 
-		var hexCode = "#";
-  	var hexValues = "0123456789abcdef";
+		let hexCode = "#";
+  	const hexValues = "0123456789abcdef";
 
-  	for (var i = 0; i < 6; i++) {
+  	for (let i = 0; i < 6; i++) {
     			hexCode += hexValues[(Math.floor(Math.random() * hexValues.length))];
   	}
 
@@ -54,28 +52,28 @@ function generateRandomHex() {
 // Convertion function (This bit with help from chatGPT)
 
 // Function to get HSL format from a hex color code
-function getHslFormat(hex) {
+const getHslFormat = (hex) => {
     // Remove the '#' from the beginning of the hex code
     hex = hex.slice(1);
 
     // Convert hex to RGB
-    var r = parseInt(hex.substring(0, 2), 16) / 255;
-    var g = parseInt(hex.substring(2, 4), 16) / 255;
-    var b = parseInt(hex.substring(4, 6), 16) / 255;
+    const r = parseInt(hex.substring(0, 2), 16) / 255;
+    const g = parseInt(hex.substring(2, 4), 16) / 255;
+    const b =  parseInt(hex.substring(4, 6), 16) / 255;
 
     // Find the minimum and maximum values
-    var cMin = Math.min(r, g, b);
-    var cMax = Math.max(r, g, b);
+   const cMin = Math.min(r, g, b);
+   const cMax = Math.max(r, g, b);
 
     // Calculate lightness
-    var l = (cMin + cMax) / 2;
+   const l = (cMin + cMax) / 2;
 
     // Calculate saturation
-    var delta = cMax - cMin;
-    var s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+const delta = cMax - cMin;
+const s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
 
     // Calculate hue
-    var h;
+    let h;
     if (delta === 0) {
         h = 0;
     } else if (cMax === r) {
@@ -87,10 +85,12 @@ function getHslFormat(hex) {
     }
 
     // Ensure hue is positive and convert it to degrees
-    h = Math.round((h * 60 + 360) % 360);
+    const roundedH = Math.round((h * 60 + 360) % 360);
+    const roundedS = Math.round(s * 100);
+    const roundedL = Math.round(l * 100);
 
     // Format HSL string
-    return "HSL(" + h + ", " + (s * 100) + "%, " + (l * 100) + "%)";
+    return `HSL ${roundedH}, ${roundedS}%, ${roundedL}%`;
 }
 
 
@@ -104,9 +104,9 @@ color2.addEventListener("input", setGradient);
 	
 	// Randomly generates new colors for background
 
-random1.addEventListener("click", function() {
+random1.addEventListener("click", () => {
   // Generate a random hex color for color1
-  var randomColor1 = generateRandomHex();
+  let randomColor1 = generateRandomHex();
 
 
   // Set the value of color1 and update the background gradient
@@ -117,9 +117,9 @@ random1.addEventListener("click", function() {
 
 });
 	
-random2.addEventListener("click", function() {
+random2.addEventListener("click", () => {
 	// Generate a random hex color for color2
-  var randomColor2 = generateRandomHex();
+  let randomColor2 = generateRandomHex();
 
   // Set the value of color2 and update the background gradient
   color2.value = randomColor2;
